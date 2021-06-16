f4e679043a92a304b7702656992b6faaa8308e7a
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-06-16 20:33:44 -0300

    Refatoraçao lint javascript

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function validateForm() {
-  const loginIsValid = document.getElementById("user-login").value;
-  const passwordIsValid = document.getElementById("user-password").value;
-  if (loginIsValid === "tryber@teste.com" && passwordIsValid === "123456") {
-    alert("Olá, Tryber!");
+  const loginIsValid = document.getElementById('user-login').value;
+  const passwordIsValid = document.getElementById('user-password').value;
+  if (loginIsValid === 'tryber@teste.com' && passwordIsValid === '123456') {
+    alert('Olá, Tryber!');
     return false;
   }
-  if (loginIsValid !== "tryber@teste.com" && passwordIsValid !== "123456") {
-    alert("Login ou senha inválidos.");
+  if (loginIsValid !== 'tryber@teste.com' && passwordIsValid !== '123456') {
+    alert('Login ou senha inválidos.');
     return false;
   }
 }
-const getBtn = document.getElementById("btn-login");
-getBtn.addEventListener("click", validateForm);
+const getBtn = document.getElementById('btn-login');
+getBtn.addEventListener('click', validateForm);
